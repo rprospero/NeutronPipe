ed1225e87be25d5cc53a6b446443f89c2bf9fd51
= <=>
2014-08-01 16:26:10 +0100

    Created first pipe beamline

--- Main.hs
+++ Main.hs
@@ -1,3 +1,17 @@
 module Main (main) where
 
-main = print "Hello, World!"
+import Control.Monad (forever)
+
+import Neutron
+import Vec
+import Pipes
+
+source :: Num a => Producer (Vec a) IO ()
+source = forever $ yield 1
+
+detector :: (Num a, Show a) => Consumer (Vec a) IO ()
+detector = forever $ do
+  temp <- await
+  lift $ print temp
+
+main = runEffect $ source >-> detector
