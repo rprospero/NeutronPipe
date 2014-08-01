6c66d39f825d2e33e66cbe1850a275bf29d336c5
= <=>
2014-08-01 17:00:56 +0100

    Added a slit to the dummy beamline

--- Main.hs
+++ Main.hs
@@ -7,16 +7,25 @@ import Neutron
 import Vec
 import Pipes
 
-source :: Producer (Vec Double) IO ()
+source :: Producer (Neutron Double) IO ()
 source = forever $ do
            g <- lift getStdGen
            let (a,g2) = random g
            yield a
            lift $ setStdGen g2
 
-detector :: (Num a, Show a) => Consumer (Vec a) IO ()
+detector :: (Num a, Show a) => Consumer (Neutron a) IO ()
 detector = forever $ do
   temp <- await
   lift $ print temp
 
-main = runEffect $ source >-> detector
+slit :: (Fractional a, Ord a) => Pipe (Neutron a) (Neutron a) IO ()
+slit = forever $ do
+         n <- await
+         if (x . position $ n) < 0.5
+         then
+             yield n
+         else
+             discard n
+
+main = runEffect $ source >-> slit >-> detector
