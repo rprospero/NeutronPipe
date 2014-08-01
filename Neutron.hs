472eb5a1f96db923aa27f4c5bb16741b1e2e3ad1
= <=>
2014-08-01 16:14:59 +0100

    Add Show to Neutron and Vec types

--- Neutron.hs
+++ Neutron.hs
@@ -1,9 +1,14 @@
-module Neutron where
+module Neutron (Neutron,advance) where
+
+import Data.List (intersperse)
 
 import Vec
 
 data Neutron a = Neutron {position :: Vec a,
                           velocity :: Vec a}
+
+instance Show a => Show (Neutron a) where
+    show n = ("Neutron " ++) . concat . intersperse " " . map show $ [position n, velocity n]
 
 advance :: Num a => a -> Neutron a -> Neutron a
 advance t n = n {position = position n + scale t (velocity n)}
--- Vec.hs
+++ Vec.hs
@@ -1,8 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Vec (Vec,dot,cross,scale) where
 
 import Control.Applicative ((<*>))
+import Data.List (intersperse)
 
 data Vec a = Vec {x :: a, y :: a, z :: a}
+
+instance Show a => Show (Vec a) where
+    show v = ("Vec " ++) . concat . intersperse " " . map show $ [x v, y v, z v]
+
+instance Num a => Num (Vec a) where
+    (+) i j = Vec (x i + x j) (y i + y j) (z i + z j)
+    (-) i j = Vec (x i - x j) (y i - y j) (z i - z j)
+    (*) = cross
+    abs i = Vec (abs (x i)) (abs (y i)) (abs (z i))
+    fromInteger i = Vec (fromInteger i) (fromInteger i) (fromInteger i)
+    signum i = Vec (signum (x i)) (signum (y i)) (signum (z i))
 
 dot :: Num a => Vec a -> Vec a -> a
 dot i j = x i * x j + y i * y j + z i * z j
@@ -14,11 +27,3 @@ cross i j = Vec (y i * z j - z i * y j)
 
 scale :: Num a => a -> Vec a -> Vec a
 scale s v = Vec (s * x v) (s * y v) (s * z v)
-
-instance Num a => Num (Vec a) where
-    (+) i j = Vec (x i + x j) (y i + y j) (z i + z j)
-    (-) i j = Vec (x i - x j) (y i - y j) (z i - z j)
-    (*) = cross
-    abs i = Vec (abs (x i)) (abs (y i)) (abs (z i))
-    fromInteger i = Vec (fromInteger i) (fromInteger i) (fromInteger i)
-    signum i = Vec (signum (x i)) (signum (y i)) (signum (z i))
