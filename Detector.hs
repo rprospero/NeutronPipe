3dee9c19416ccf639c06502f0ce4aa70ef08863d
Adam Washington <rprospero@gmail.com>
2014-08-25 22:55:51 +0100

    Add quite a bit to the detector setup

--- Detector.hs
+++ Detector.hs
@@ -1,10 +1,13 @@
-module Detector (detector) where
+module Detector (detector,dumpToFile) where
 
 import Pipes
+import Pipes.Lift
 import Neutron
+import Vec
 
 import Control.Monad(forever)
 import Data.Default.Class (def)
+import Control.Monad.Trans.State.Strict
 
 import Graphics.Rendering.Chart
 import Graphics.Rendering.Chart.Gtk
@@ -12,12 +15,21 @@ import Data.Colour
 import Data.Colour.Names
 import Control.Lens
 
-import Data.Vector.Unboxed ((!),(//),replicate)
+import Data.Vector.Unboxed ((!),(//),replicate,toList)
+
+import System.IO
 
 detector :: (Num a, Show a) => Consumer (Neutron a) IO ()
 detector = forever $ do
   temp <- await
   lift $ print temp
+
+dumpToFile :: (Show a) => FilePath -> Consumer a IO ()
+dumpToFile file = forever $ do
+                    temp <- await
+                    h <- lift $ openFile file AppendMode
+                    lift $ hPutStr h . (++ "\n") . show $ temp
+                    lift $ hClose h
 
 norm :: Double -> Double
 norm = exp . (* (-1) ) . (\x -> x*x)
@@ -32,17 +44,55 @@ chart = toRenderable layout
               $ plot_lines_style  . line_color .~ opaque blue
               $ plot_lines_title .~ "am"
               $ def
+
 incrIndex index value = value // [(index,1+(value!index))]
 
-binner
-  :: (RealFrac s) => Int -> [s] -> s -> Int
-binner size xs value = floor $ (fromIntegral size) * (value - small) / ((large-small) * 101/100)
-    where
-      small = Prelude.minimum xs
-      large = Prelude.maximum xs
+binner' :: Monad m => Int -> Pipe a [a] (StateT [a] m) b
+binner' bins = forever $
+         do
+           next <- await
+           current <- lift $ get
+           if length current >= bins
+           then do
+             lift $ put $ []
+             yield . reverse $ [next] ++ current
+           else do
+             lift $ put $ [next] ++ current
 
-histlist size xs = Prelude.foldr (incrIndex . f) (Data.Vector.Unboxed.replicate size 0) $ xs
-                where f = binner size xs
+binner :: Monad m => Int -> Pipe t [t] m r
+binner bins = evalStateP [] $ binner' bins
 
-test :: IO ()
-test = renderableToWindow chart 100 100
+pipeOver' :: Monad m => (a -> b -> b) -> Pipe a b (StateT b m )r
+pipeOver' f =  forever $
+         do
+           next <- await
+           current <- lift $ get
+           let result = f next current
+           lift $ put $ result
+           yield result
+
+
+pipeOver :: Monad m => b -> (a -> b -> b) -> 
+            Pipe a b m r
+pipeOver initial = evalStateP initial . pipeOver'
+
+
+test chart = renderableToWindow chart 100 100
+
+
+-- histogram size xs = toRenderable layout
+--     where
+--       layout = layout_title .~ "Amplitude Modulation"
+--            $ layout_plots .~ [plotBars sinusoid1]
+--            $ def
+--       sinusoid1 = plot_bars_values .~ (zip [0..size] $  map (\x -> [x]) histed)
+--               $ def
+--       histed = (toList . histlist size $ xs) :: [Int]
+
+
+
+xcoord :: Pipe (Neutron a) a IO ()
+xcoord = forever $ do
+           temp <- await
+           yield $ x . position $ temp
+
