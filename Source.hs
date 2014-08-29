2d685f7d8b8f96b6aab5e4be018f8e9ce09d3786
= <=>
2014-08-29 17:19:21 +0100

    Start work on implementing McStas's simple source

--- Source.hs
+++ Source.hs
@@ -1,10 +1,12 @@
-module Source (source) where
+module Source (source,simpleSource) where
 
 import Control.Monad (forever)
 import Metropolis (metropolis)
 import Pipes
 import Neutron
+import Vec
 import System.Random
+import Control.Monad.Random.Class
 
 generalSource p v = forever $ do
                       pos <- lift $ metropolis p
@@ -17,3 +19,19 @@ source = forever $ do
            let (a,g2) = random g
            yield a
            lift $ setStdGen g2
+
+data Area a = Circle a | Rect a a
+
+inArea :: MonadRandom m => Area Double -> m (Vec Double)
+inArea (Circle r) = do
+  rho <- getRandom
+  phi <- getRandom
+  let x0 = r * rho * cos (2*pi*phi)
+  let y0 = r * rho * sin (2*pi*phi)
+  return (Vec x0 y0 0)
+
+simpleSource startArea targetArea distance = forever $
+                                             do
+                                               start <- lift $ inArea startArea
+                                               target <- lift $ inArea targetArea
+                                               yield (Neutron start 1 target)
