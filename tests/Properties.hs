d692e4a0be246e54e8a0fbe60f5b42916173e591
Adam Washington <rprospero@gmail.com>
2014-09-11 20:24:20 +0100

    Update test script to use Tasty

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,39 +1,41 @@
-import Test.Framework (defaultMain, testGroup)
-import Test.Framework.Providers.HUnit
-import Test.Framework.Providers.QuickCheck2 (testProperty)
-
-import Test.HUnit
-import Test.QuickCheck
-
+import Test.Tasty
+import Test.Tasty.QuickCheck
 import Vec
 
---We're going to use ints because they escape the issues with floating point
-arbitraryVec :: Gen (Vec Int)
-arbitraryVec = choose (Vec (-1) (-1) (-1),
-                       Vec 1 1 1)
+-- --We're going to use ints because they escape the issues with floating point
+-- arbitraryVec :: Gen (Vec Int)
+-- arbitraryVec = choose (Vec (-1) (-1) (-1),
+--                        Vec 1 1 1)
 
-tripToTup :: [t] -> (t, t, t)
-tripToTup (a:b:c:xs) = (a,b,c)
+-- tripToTup :: [t] -> (t, t, t)
+-- tripToTup (a:b:c:xs) = (a,b,c)
 
-arbitraryTriple = fmap tripToTup $ suchThat (listOf arbitraryVec) (\x -> length x > 3)
+-- arbitraryTriple = fmap tripToTup $ suchThat (listOf arbitraryVec) (\x -> length x > 3)
 
-testZero :: (Show a, Num a, Eq a) => a -> Property
-testZero xs = (xs + 0) === xs
+-- testZero :: (Show a, Num a, Eq a) => a -> Property
+-- testZero xs = (xs + 0) === xs
 
-testDouble :: (Show a, Num a, Eq a) => Vec a -> Property
-testDouble xs = xs + xs === scale 2 xs
+-- testDouble :: (Show a, Num a, Eq a) => Vec a -> Property
+-- testDouble xs = xs + xs === scale 2 xs
 
-testTriple
-  :: (Show a, Num a, Eq a) => (Vec a, Vec a, Vec a) -> Property
-testTriple (a, b, c) = a * (b * c) === scale (a `dot` c) b - scale (a `dot` b) c
+-- testTriple
+--   :: (Show a, Num a, Eq a) => (Vec a, Vec a, Vec a) -> Property
+-- testTriple (a, b, c) = a * (b * c) === scale (a `dot` c) b - scale (a `dot` b) c
 
-testCross :: (Show a, Num a, Eq a) => a -> Property
-testCross xs = xs * xs === 0
+-- testCross :: (Show a, Num a, Eq a) => a -> Property
+-- testCross xs = xs * xs === 0
 
-tests = [testProperty "Vector Addition Identity" $ forAll arbitraryVec testZero,
-        testProperty "Vector Scaling" $ forAll arbitraryVec testDouble,
-        testProperty "Cross Self is Zero" $ forAll arbitraryVec testCross,
-        testProperty "Vector Triple Product" $ forAll arbitraryTriple testTriple]
+-- tests = [testProperty "Vector Addition Identity" $ forAll arbitraryVec testZero,
+--         testProperty "Vector Scaling" $ forAll arbitraryVec testDouble,
+--         testProperty "Cross Self is Zero" $ forAll arbitraryVec testCross,
+--         testProperty "Vector Triple Product" $ forAll arbitraryTriple testTriple]
+
+crossTest :: Int -> Int -> Int -> Bool
+crossTest a b c = x * x == 0
+    where
+      x = Vec a b c
+
+tests = testGroup "Tests" [testProperty "Cross" $ crossTest]
 
 main :: IO ()
 main = defaultMain tests
