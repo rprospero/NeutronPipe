738d0c9c4c43701ed3111d985e8478c51e360fab
Adam Washington <rprospero@gmail.com>
2014-09-11 20:43:10 +0100

    Re-implement test suite in stupid way

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -2,40 +2,27 @@ import Test.Tasty
 import Test.Tasty.QuickCheck
 import Vec
 
--- --We're going to use ints because they escape the issues with floating point
--- arbitraryVec :: Gen (Vec Int)
--- arbitraryVec = choose (Vec (-1) (-1) (-1),
---                        Vec 1 1 1)
+testDouble :: Int -> Int -> Int -> Bool
+testDouble a b c = xs + xs == scale 2 xs
+    where
+      xs = Vec a b c
 
--- tripToTup :: [t] -> (t, t, t)
--- tripToTup (a:b:c:xs) = (a,b,c)
-
--- arbitraryTriple = fmap tripToTup $ suchThat (listOf arbitraryVec) (\x -> length x > 3)
-
--- testZero :: (Show a, Num a, Eq a) => a -> Property
--- testZero xs = (xs + 0) === xs
-
--- testDouble :: (Show a, Num a, Eq a) => Vec a -> Property
--- testDouble xs = xs + xs === scale 2 xs
-
--- testTriple
---   :: (Show a, Num a, Eq a) => (Vec a, Vec a, Vec a) -> Property
--- testTriple (a, b, c) = a * (b * c) === scale (a `dot` c) b - scale (a `dot` b) c
-
--- testCross :: (Show a, Num a, Eq a) => a -> Property
--- testCross xs = xs * xs === 0
-
--- tests = [testProperty "Vector Addition Identity" $ forAll arbitraryVec testZero,
---         testProperty "Vector Scaling" $ forAll arbitraryVec testDouble,
---         testProperty "Cross Self is Zero" $ forAll arbitraryVec testCross,
---         testProperty "Vector Triple Product" $ forAll arbitraryTriple testTriple]
+testTriple
+  :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Bool
+testTriple ax ay az bx by bz cx cy cz  = a * (b * c) == scale (a `dot` c) b - scale (a `dot` b) c
+    where
+      a = Vec ax ay az
+      b = Vec bx by bz
+      c = Vec cx cy cz
 
 crossTest :: Int -> Int -> Int -> Bool
 crossTest a b c = x * x == 0
     where
       x = Vec a b c
 
-tests = testGroup "Tests" [testProperty "Cross" $ crossTest]
+tests = testGroup "Tests" [testProperty "Cross" $ crossTest,
+                          testProperty "Triple" $ testTriple,
+                          testProperty "Scale" $ testDouble]
 
 main :: IO ()
 main = defaultMain tests
