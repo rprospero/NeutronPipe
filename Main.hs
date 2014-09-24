3838480700b27caf074ff73d304ba7241b69fc55
= <=>
2014-09-24 14:04:29 +0100

    Two changes
    1: Switch to PureMT for random source.  This gives a factor of two speed
       boost
    2: Directly calculate neutron vector positions, replacing old Area system

--- Main.hs
+++ Main.hs
@@ -12,48 +12,42 @@ module Main (main) where
 
 import Neutron (getEnergy)
 import Momentum (Energy(Energy),Momentum)
-import Linear
 import Pipes
 import qualified Pipes.Prelude as P
 
 import Slits (slit)
-import Detector (dumpToConsole,histBuilder,pushEvery,histPipe)
-import Source (simpleSource,Area(Rect,Circle))
+import Detector (dumpToConsole,histBuilder)
+import Source (simpleSource)
 import Control.Applicative
-import Data.Random    
-import Data.Traversable
+import Data.Random
+import Data.Random.Source.PureMT
+import Data.IORef
 import Control.Comonad(extract)
 import Control.Monad (liftM)
+import Linear (V3(V3))
 
---startbox :: Area Double
---startbox = Rect 0 1 0 1
---targetbox :: Area Double
---targetbox = Circle 1
+startbox :: RVar (V3 Double)
+startbox = V3 <$> uniform 0 1 <*> uniform 0 1 <*> pure 0
 
-startbox :: RVar (Double, Double)
-startbox = do
-  x <- uniform 0 1
-  y <- uniform 0 1
-  return (x,y)
-
-targetbox :: RVar (Double, Double)
+targetbox :: RVar (V3 Double)
 targetbox = do
   rho <- uniform 0 1
   phi <- uniform 0 (2*pi)
   let x0 = rho * cos phi
   let y0 = rho * sin phi
-  return (x0,y0)
+  return $ V3 x0 y0 1
 
 mySpread :: RVar (Energy Double)
 mySpread = liftM Energy $ normal 1.0 0.1
-
-main :: IO ()
+           
+--main' :: (RandomSource IO s) => s -> IO ()
 -- | Simulate the beamline
-main = runEffect $ simpleSource startbox targetbox 1 mySpread >-> 
-       slit (V3 0 0 (-10)) (V3 0.4 0.9 10) >->
-       P.take 100000 >->
---       histPipe (extract.getEnergy) 40 (0,2) >-> 
---       pushEvery 50000 >->
-       histBuilder (extract.getEnergy) 40 (0,2) 50000 >->
-       dumpToConsole
---       P.drain
+main' src = runEffect $ simpleSource src startbox targetbox mySpread >-> 
+            slit (V3 0 0 (-10)) (V3 0.4 0.9 10) >->
+            P.take 100000 >->
+            histBuilder (extract.getEnergy) 40 (0,2) 50000 >->
+            dumpToConsole
+
+main = do
+  src <- newIORef (pureMT 1234)
+  main' src
--- Source.hs
+++ Source.hs
@@ -1,37 +1,18 @@
 {-# LANGUAGE FlexibleContexts #-}
-module Source (simpleSource,Area(Circle,Rect)) where
+module Source (simpleSource) where
 
 import Control.Monad (forever)
 import Pipes
 import Neutron (Neutron(Neutron))
-import Linear (normalize,V3(V3),Epsilon,(*^))
-import Data.Random
+import Linear (normalize,V3,Epsilon,(*^))
+import Data.Random (sample,sampleFrom,RVar,RandomSource)
 import Momentum (Momentum(getSpeed),Energy,Speed,Wavelength)
-import Data.Traversable
 
-data Area a = Circle a | Rect a a a a
-
-inArea :: (MonadRandom m, Floating a, Distribution Uniform a) => Area a -> a -> m (V3 a)
-inArea (Circle r) d = do
-  rho <- sample $ uniform 0 r
-  phi <- sample $ uniform 0 (2*pi)
-  let x0 = rho * cos phi
-  let y0 = rho * sin phi
-  return (V3 x0 y0 d)
-inArea (Rect bottom top left right) d = do
-  y0 <- sample $ uniform bottom top
-  x0 <- sample $ uniform left right
-  return (V3 x0 y0 d)
-
---samplePoint 
-
-{-# SPECIALIZE simpleSource :: RVar (Double, Double) -> RVar (Double, Double) -> Double -> RVar (Energy Double) -> Producer (Neutron Double) IO () #-}
-simpleSource :: (Epsilon a, Num a, Distribution Uniform a, Floating a, Momentum m) => RVar (a,a) -> RVar (a,a) -> a -> RVar (m a) -> Producer (Neutron a) IO ()
-simpleSource startArea targetArea distance momentumSpread = forever $
+{-# SPECIALIZE simpleSource :: (RandomSource IO s) => s -> RVar (V3 Double) -> RVar (V3 Double) -> RVar (Energy Double) -> Producer (Neutron Double) IO () #-}
+simpleSource :: (RandomSource IO s, Epsilon a, Num a, Floating a, Momentum m) => s -> RVar (V3 a) -> RVar (V3 a) -> RVar (m a) -> Producer (Neutron a) IO ()
+simpleSource src startArea targetArea momentumSpread = forever $
              do
-               spread <- lift . sample $ momentumSpread
-               (x1,y1) <- lift . sample $ startArea
-               (x2,y2) <- lift . sample $ targetArea
-               let start = V3 x1 y1 0
-               let target = V3 x2 y2 distance
+               spread <- lift . sampleFrom src $ momentumSpread
+               start <- lift . sampleFrom src $ startArea
+               target <- lift . sampleFrom src $ targetArea
                yield . Neutron start 1 . (getSpeed spread *^). normalize $ (target-start)
