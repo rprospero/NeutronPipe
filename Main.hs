f10ea842675009eb3b847454abf0f61cefb6ff07
= <=>
2014-09-30 16:47:35 +0100

    Organise the construction of a model

--- Main.hs
+++ Main.hs
@@ -25,6 +25,19 @@ import Data.Random.Source.PureMT
 import Data.IORef
 import Control.Comonad(extract)
 import Linear (V3(V3))
+import Control.Monad (liftM)
+
+-- Step 1: Define the beamline
+-- Make a beamline function with parameters for every random value
+
+startSlit :: Neutron Double -> Maybe (Neutron Double)
+startSlit = slit (V3 0 0 (-10)) (V3 0.4 0.9 10)
+
+beamline :: (Momentum m) => V3 Double -> V3 Double -> m Double -> Maybe (Neutron Double)
+beamline start target momentum = startSlit $ simpleSource start target momentum
+
+-- Step 2: Define Random Variables
+-- Make a random variable for each of the values
 
 startbox :: RVar (V3 Double)
 startbox = V3 <$> uniform 0 1 <*> uniform 0 1 <*> pure 0
@@ -39,22 +52,21 @@ targetbox = do
 
 mySpread :: RVar (Energy Double)
 mySpread = liftM Energy $ normal 1.0 0.5
-           
+
+-- Step 3: Make a random beamline
+-- This should have NO parameters and a type of RVar (Maybe (Neutron Double))
+
+beam :: RVar (Maybe (Neutron Double))
+beam = beamline <$> startbox <*> targetbox <*> mySpread
+
+-- Step 4: Run the beamline!
+
 main' :: (RandomSource IO s) => s -> IO ()
 -- | Simulate the beamline
 main' src = runEffect $ producer src beam >->
             P.take 1000000 >->
             histBuilder (extract.getEnergy) 40 (0,2) 50000 >->
             dumpToConsole
-
-startSlit :: Neutron Double -> Maybe (Neutron Double)
-startSlit = slit (V3 0 0 (-10)) (V3 0.4 0.9 10)
-
-beamline :: (Momentum m) => V3 Double -> V3 Double -> m Double -> Maybe (Neutron Double)
-beamline start target momentum = startSlit $ simpleSource start target momentum
-
-beam :: RVar (Maybe (Neutron Double))
-beam = beamline <$> startbox <*> targetbox <*> mySpread
 
 main :: IO ()
 main = do
