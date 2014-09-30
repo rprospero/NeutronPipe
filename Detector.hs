841d80c78c83f97f3f4c7e37537142f62e6da486
= <=>
2014-09-30 16:35:45 +0100

    Remove cruft from Detector.hs

--- Detector.hs
+++ Detector.hs
@@ -1,23 +1,12 @@
-module Detector (detector,dumpToFile,dumpToConsole,pipeOver,
-                         xcoord,binner,histPipe,pushEvery,histBuilder) where
+module Detector (dumpToFile,dumpToConsole,histBuilder) where
 
 import Pipes
-import Pipes.Lift
-import qualified Pipes.Prelude as P
-import Neutron
-import Linear
 
 import Control.Monad(forever)
-import Control.Monad.Trans.State.Strict
 
 import qualified Data.Vector.Unboxed as V
 
 import System.IO
-
-detector :: (Num a, Show a) => Consumer (Neutron a) IO ()
-detector = forever $ do
-  temp <- await
-  lift $ print temp
 
 dumpToFile :: (Show a) => FilePath -> Consumer a IO ()
 dumpToFile file = forever $ do
@@ -30,35 +19,6 @@ dumpToConsole :: (Show a) => Consumer a IO ()
 dumpToConsole = forever $ do
                   temp <- await
                   lift $ print . (++ "\n") . show $ temp
-
-binner' :: Monad m => Int -> Pipe a [a] (StateT [a] m) b
-binner' bins = forever $
-         do
-           next <- await
-           current <- lift get
-           if length current >= bins
-           then do
-             lift $ put []
-             yield . reverse $ next : current
-           else
-               lift $ put $ next : current
-
-binner :: Monad m => Int -> Pipe t [t] m r
-binner bins = evalStateP [] $ binner' bins
-
-pipeOver :: Monad m => b -> (a -> b -> b) -> 
-            Pipe a b m r
---pipeOver initial = evalStateP initial . pipeOver'
-pipeOver initial f = P.scan (flip f) initial id
-
-x (V3 a _ _) = a
-
-xcoord :: Pipe (Neutron a) a IO ()
-xcoord = forever $ do
-           temp <- await
-           yield $ x . position $ temp
-
-
 
 --- Pipe for performing histogramming
 
@@ -73,12 +33,6 @@ toBin steps (low,high) n = let temp = fromIntegral steps * (n-low)/(high-low)
                                 then steps-1
                                 else round temp
 
-histPipe :: Monad m => (a -> Double) -> Int -> (Double,Double) -> Pipe a (V.Vector Int) m r
-histPipe f bins range = pipeOver zeroList updater
-    where
-      zeroList = V.replicate bins 0
-      updater = updateVector bins (toBin bins range) . f
-
 updateVector :: Int -> (Double->Int) -> Double -> V.Vector Int -> V.Vector Int
 updateVector bins f n v = v V.// [(idx, 1+(v V.! idx))]
     where
@@ -87,13 +41,6 @@ updateVector bins f n v = v V.// [(idx, 1+(v V.! idx))]
           | ix > bins = bins - 1
           | otherwise = ix
       ix = f n
-
-pushEvery :: (Monad m) => Int -> Pipe a a m r
-pushEvery n = pushEvery' n n
-
-pushEvery' :: (Monad m) => Int -> Int -> Pipe a a m r
-pushEvery' n 0 = await >>= yield >> pushEvery' n n
-pushEvery' n m = await >> pushEvery' n (m-1)
 
 histBuilder :: Monad m => (a -> Double) -> Int -> (Double,Double) -> Int -> Pipe a (V.Vector Int) m r
 histBuilder f bins range delay = histBuilder' updater delay delay zeroList
