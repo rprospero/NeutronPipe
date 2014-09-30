3a4cf14ae3621dcc4b07a5ebbac8c8e41acec46d
= <=>
2014-09-30 17:25:38 +0100

    Start on doing arrays with RVars

--- Main.hs
+++ Main.hs
@@ -8,7 +8,7 @@ Stability   : experimental
 Portability : POSIX
 
 This module performs a monte-carlo simulation of a neutron beamline.-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances #-}
 module Main (main) where
 
 import Neutron (getEnergy,Neutron)
@@ -24,25 +24,58 @@ import Data.Random
 import Data.Random.Source.PureMT
 import Data.IORef
 import Control.Comonad(extract)
-import Linear (V3(V3))
+import Linear (V3(V3),Epsilon,nearZero)
 import Control.Monad (liftM)
+
+import Data.Vector as V
 
 -- Step 1: Define the beamline
 -- Make a beamline function with parameters for every random value
 
-startSlit :: Neutron Double -> Maybe (Neutron Double)
+instance Num (V.Vector Double) where
+    (+) = V.zipWith (+)
+    (-) = V.zipWith (-)
+    (*) = V.zipWith (*)
+    abs = V.map abs
+    signum = V.map signum
+    fromInteger = V.replicate chunksize . fromInteger
+instance Fractional (V.Vector Double) where
+    (/) = V.zipWith (/)
+    fromRational = V.replicate chunksize . fromRational
+instance Floating (V.Vector Double) where
+    sin = V.map sin
+    cos = V.map cos
+    log = V.map log
+    exp = V.map exp
+    asin = V.map asin
+    atan = V.map atan
+    acos = V.map acos
+    sinh = V.map sinh
+    cosh = V.map cosh
+    asinh = V.map asinh
+    acosh = V.map acosh
+    atanh = V.map atanh
+    pi = V.replicate chunksize pi
+instance Epsilon (V.Vector Double) where
+    nearZero = const False
+
+chunksize :: Int
+chunksize = 1000
+
+startSlit :: Neutron (V.Vector Double) -> Maybe (Neutron (V.Vector Double))
 startSlit = slit (V3 0 0 (-10)) (V3 0.4 0.9 10)
 
-beamline :: (Momentum m) => V3 Double -> V3 Double -> m Double -> Maybe (Neutron Double)
+beamline :: (Momentum m) => V3 (V.Vector Double) -> V3 (V.Vector Double) -> m (V.Vector Double) -> Maybe (Neutron (V.Vector Double))
 beamline start target momentum = startSlit $ simpleSource start target momentum
+--beamline = error "Fail"
 
 -- Step 2: Define Random Variables
 -- Make a random variable for each of the values
 
-startbox :: RVar (V3 Double)
+startbox :: RVar (V3 (V.Vector Double))
 startbox = V3 <$> uniform 0 1 <*> uniform 0 1 <*> pure 0
 
-targetbox :: RVar (V3 Double)
+targetbox :: RVar (V3 (V.Vector Double))
 targetbox = do
   rho <- uniform 0 1
   phi <- uniform 0 (2*pi)
@@ -50,14 +83,15 @@ targetbox = do
   let y0 = rho * sin phi
   return $ V3 x0 y0 1
 
-mySpread :: RVar (Energy Double)
+mySpread :: RVar (Energy (V.Vector Double))
 mySpread = liftM Energy $ normal 1.0 0.5
 
 -- Step 3: Make a random beamline
 -- This should have NO parameters and a type of RVar (Maybe (Neutron Double))
 
-beam :: RVar (Maybe (Neutron Double))
+beam :: RVar (Maybe (Neutron (V.Vector Double)))
 beam = beamline <$> startbox <*> targetbox <*> mySpread
+--beam = error "Fail"
 
 -- Step 4: Run the beamline!
 
@@ -65,7 +99,7 @@ main' :: (RandomSource IO s) => s -> IO ()
 -- | Simulate the beamline
 main' src = runEffect $ producer src beam >->
             P.take 1000000 >->
-            histBuilder (extract.getEnergy) 40 (0,2) 50000 >->
+--            histBuilder (extract.getEnergy) 40 (0,2) 50000 >->
             dumpToConsole
 
 main :: IO ()
