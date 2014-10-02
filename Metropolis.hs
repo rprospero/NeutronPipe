90fd906689f259f2e907959df49c90d67c7d0b04
Adam Washington <rprospero@gmail.com>
2014-10-02 21:00:47 +0100

    Generalise metropolis

--- Metropolis.hs
+++ Metropolis.hs
@@ -1,13 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Metropolis (metropolis) where
 
 import Data.Random
 
-metropolis :: (Double -> Double) -> Int -> RVar Double
+metropolis :: (Distribution Uniform a, Distribution Uniform b, Ord a, Num b, Fractional a) => (b -> a) -> Int -> RVar b
 metropolis f count = last . take count $ scanl (>>=) (step 1) (repeat step)
     where
       step = met' f
 
-met' :: (Double -> Double) -> Double -> RVar Double
+met' :: (Distribution Uniform a, Distribution Uniform b, Ord a, Num b, Fractional a) => (b -> a) -> b -> RVar b
 met' f xold = do
   step <- uniform (-1) 1
   let original = f xold
