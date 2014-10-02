e0ca25bf6f1475aa6700f801a520acf562c2ff22
Adam Washington <rprospero@gmail.com>
2014-10-02 20:51:15 +0100

    Moved metropolis into an RVar

--- Metropolis.hs
+++ Metropolis.hs
@@ -1,29 +1,20 @@
 module Metropolis (metropolis) where
 
-import System.Random
-import Control.Monad.Random.Class
+import Data.Random
 
-metropolis
-  :: (MonadRandom m, Random a, Random b, Ord a, Num b,
-      Fractional a) =>
-     (b -> a) -> m [b]
-metropolis f = do
-  base <- getRandom
-  steps <- getRandoms
-  tests <- getRandoms
-  return $ scanl (met' f) base $ zip (map rescale steps) tests
+metropolis :: (Double -> Double) -> Int -> RVar Double
+metropolis f count = last . take count $ scanl (>>=) (step 1) (repeat step)
+    where
+      step = met' f
 
-rescale :: Num a => a -> a
-rescale x = 2 * x - 1
+met' :: (Double -> Double) -> Double -> RVar Double
+met' f xold = do
+  step <- uniform (-1) 1
+  let original = f xold
+  let xnew = xold + step
+  let fnew = f xnew
+  test <- uniform 0 1
+  return $ if fnew/original > test
+           then xnew
+           else xold
 
-met'
-  :: (Ord a, Num a1, Fractional a) =>
-     (a1 -> a) -> a1 -> (a1, a) -> a1
-met' f xold (xstep,test) = let yold = f xold
-                               ynew = f $ xold + xstep
-                           in
-                             if ynew/yold > test
-                             then
-                                 xold + xstep
-                             else
-                                 xold
