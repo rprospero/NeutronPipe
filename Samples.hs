93e5bad2628b9e5d946f2ac7d046f7b69bec959d
= <=>
2014-10-03 14:17:37 +0100

    Make scattering vector perpendicular to direction

--- Samples.hs
+++ Samples.hs
@@ -1,7 +1,11 @@
 module Samples (scatter) where
 
 import Neutron
-import Linear.V3
+import Linear (V3(V3),normalize,cross,Epsilon,(*^))
 
-scatter :: Floating a => a -> Neutron a -> Neutron a
-scatter angle n = n {direction = V3 (cos angle) (sin angle) 0 + direction n}
+scatter :: (Epsilon a, Floating a) => a -> Neutron a -> Neutron a
+scatter angle n = n {direction = dir + cos angle *^ v1 + sin angle *^ v2 }
+    where
+      dir = direction n
+      v1 = normalize . cross dir $ V3 1 0 0
+      v2 = normalize . cross dir $ V3 0 1 0
